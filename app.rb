ba6972834c30a063a2becf04c428b0d39dcc8e77
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-07-12 22:08:59 +0000

    Done.

--- app.rb
+++ app.rb
@@ -15,28 +15,36 @@ class App < Sinatra::Base
   end
 
   get '/first_exercise' do
-    "Your first exercise will be to set your session key-value pair.\nIn the route: get '/set', write a line of code that sets the :foo key of the session hash equal to 'hello'.\nThen, naviate to the '/set' path."
+    "Your first exercise will be to set your session key-value pair.\n
+    In the route: get '/set', write a line of code that sets the :foo key of the session hash equal to 'hello'.
+    \nThen, naviate to the '/set' path."
   end
 
   get '/set' do
+    session[:foo] = 'hello'
     # set the :foo key of the session hash equal to 'hello' here!
     if session[:foo] == 'hello'
       redirect '/fetch'
     else
       "Session value has not been set!"
     end
+
+    erb :set
   end
 
   get '/fetch' do
-    "You did it! session[:foo] value: #{session[:foo]}.\nMove on to Part II of this lab at '/second_exercise' "
+    "You did it! session[:foo] value: #{session[:foo]}.\n
+    Move on to Part II of this lab at '/second_exercise' "
   end
 
   get '/second_exercise' do
-    "Your second lesson will be to set your session :id key to a value.\nIn the route: /set_session, write a line of code that sets the session[:id] equal to 1.\nThen, navigate to the '/set_session' path"
+    "Your second lesson will be to set your session :id key to a value.\n
+    In the route: /set_session, write a line of code that sets the session[:id] equal to 1.\n
+    Then, navigate to the '/set_session' path"
   end
 
   get '/set_session' do
-    #set session id here
+    session[:id] = 1
 
     if session[:id] == 1
       # "Session ID set. It's currently set to #{session[:id]}."
@@ -44,15 +52,22 @@ class App < Sinatra::Base
     else
       "Session ID has not been set!"
     end
+
+    erb :set_session
   end
 
   get '/fetch_session_id' do
-    "You did it! session[:id] value: #{session[:id]}.\nNow, clear the session in the '/logout' route.\nSee the readme for further instructions.\nThen, navigate to the '/logout' path."
+    "You did it! session[:id] value: #{session[:id]}.\n
+    Now, clear the session in the '/logout' route.\n
+    See the readme for further instructions.\n
+    Then, navigate to the '/logout' path."
   end
 
   get '/logout' do
-    #clear session hash here
+    session[:id].clear
     "Session has now been cleared. session content: #{session.inspect}. Continue on to the '/finish' line!"
+
+    erb :logout
   end
 
   get '/finish' do
